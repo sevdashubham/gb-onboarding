243d61209d09e439657a5f5ad124420791a6acdb
shubham sevda <sevdashubham@gmail.com>
2020-06-23 09:34:24 +0530

    ios mobile viewport updated

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,20 @@
 import Head from 'next/head';
-import React from 'react';
+import React, { useEffect } from 'react';
 import appConstants from '../util/enUS';
 import utilStyles from '../styles/styles.module.css'
 import Stepper from "../components/stepper";
 
 export default function Home() {
+
+    useEffect(() => {
+        let vh = window.innerHeight * 0.01;
+        document.documentElement.style.setProperty('--vh', `${vh}px`);
+        window.addEventListener('resize', () => {
+            document.querySelector(':root').style
+                .setProperty('--vh', window.innerHeight/100 + 'px');
+        })
+    });
+
   const {title, description, options} = appConstants;
 
   const listOptions = options.map((option,index) => {
