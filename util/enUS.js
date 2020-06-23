7b646a3bf32ca61e33498bbae31e60abbc957fe9
shubham sevda <sevdashubham@gmail.com>
2020-06-23 07:55:13 +0530

    container with columns complete, stepper component pending

---
+++ util/enUS.js
@@ -0,0 +1,10 @@
+const appConstants = {
+    title: 'What are your main areas of focus?',
+    description: 'This will help us build a personalized experience for you',
+    options: ['Blood Glucose', 'Medication', 'Food Tracking', 'Exercise', 'Weight Management', 'Blood Pressure', 'A1C'],
+    buttonPrevious: 'Back',
+    buttonNext: 'Next',
+    buttonComplete: 'Done'
+}
+
+export default appConstants
